113335a32af0878f485aaa721288b71c9a3c8282
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-18 18:27:41 +0600

    add get method por single appointment slot booked

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try{
         await client.connect();
         const serviceCollection = client.db('Doctors_Portal').collection('services');
+        const bookingCollection = client.db('Doctors_Portal').collection('bookings');
 
         app.get('/service', async (req,res) => {
             const query = {};
@@ -23,9 +24,40 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        app.get('/available', async (req,res) =>{
+          const date = req.query.date;
+
+           const services = await serviceCollection.find().toArray();
+
+           const query = {date: date};
+           const bookings = await bookingCollection.find(query).toArray();
+          //  find booking for each service 
+          services.forEach(service =>{
+            const serviceBookings = bookings.filter(b => b.treatment === service.name);
+            const booked = serviceBookings.map(s => s.slot);
+            const available = service.slots.filter(s => !booked.includes(s))
+            service.slots = available;
+          })
+
+          res.send(services);
+        })
+
+
+        app.post('/booking', async (req,res) =>{
+          const booking = req.body;
+          const query = {treatment : booking.treatment, date: booking.date, patient: booking.patient }
+          const exists = await bookingCollection.findOne(query)
+          if(exists){
+            return res.send({success:false, booking: exists})
+          }
+          const result = await bookingCollection.insertOne(booking);
+          res.send({success:true,result});
+        })
+
+
+
     }
-
-
     finally {
        
       }
